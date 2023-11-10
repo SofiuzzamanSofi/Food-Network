c56d1597e25ac52b19b96ce23ebd8932737572ab
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-11-10 10:20:50 +0600

    screen 2  page ok

--- Frontend/pages/screen2.tsx
+++ Frontend/pages/screen2.tsx
@@ -1,92 +1,34 @@
 import type { NextPage } from "next";
-import { useState, useCallback } from "react";
-import { useRouter } from "next/router";
-import Property1Default from "../components/property1-default";
+import { useState } from "react";
 import Header from "../components/header";
-import name1Icon from "../public/name3@2x.png";
-import name2Icon from "../public/name4@2x.png";
-import name3Icon from "../public/name5@2x.png";
-import name4Icon from "../public/name6@2x.png";
-import name5Icon from "../public/name7@2x.png";
-import name6Icon from "../public/name8@2x.png";
-import name7Icon from "../public/name9@2x.png";
-import name8Icon from "../public/name10@2x.png";
-import name9Icon from "../public/name11@2x.png";
-import name10Icon from "../public/name12@2x.png";
 import MobileBottomMenu from "../components/MobileBottomMenu";
-import HomePage from "../components/HomePage";
 import LeftMenuDextop from "../components/LeftMenuDextop";
 import SettingComponent from "../components/SettingComponent";
-import MobileArrow from "../components/MobileArrow";
-import LeftListMobileMenu from "../components/LeftListMobileMenu";
 import EmptyPage from "../components/EmptyPage";
 
 
-
 const Screen2: NextPage = () => {
-    const [showPassword, setShowPassword] = useState<boolean>(false);
     const [settingMenu, setSettingMenu] = useState<boolean>(false);
-    const [mobileListMenu, setMobileListMenu] = useState<boolean>(false);
-    const router = useRouter();
-    const handleShowPasswordClick = () => {
-        setShowPassword(!showPassword);
-    };
-
-    const onIcons1Click = useCallback(() => {
-        router.push("/");
-    }, [router]);
-
-    console.log('mobileListMenu:', mobileListMenu);
-    // console.log('settingMenu:', settingMenu);
 
     return (
         <div className="relative mx-auto max-w-[120rem] min-h-screen bg-white overflow-scroll ">
 
             {/* upper-header  */}
             <Header setSettingMenu={setSettingMenu} />
-
-            <MobileArrow setMobileListMenu={setMobileListMenu} />
-
-            {
-                mobileListMenu &&
-                <LeftListMobileMenu
-                    name1={name1Icon}
-                    name2={name2Icon}
-                    name3={name3Icon}
-                    name4={name4Icon}
-                    name5={name5Icon}
-                    name6={name6Icon}
-                    name7={name7Icon}
-                    name8={name8Icon}
-                    name9={name9Icon}
-                    name10={name10Icon}
-                    // leftListMobileMenuPosition="absolute"
-                    // leftListMobileMenuTop="72px"
-                    // leftListMobileMenuLeft="84.8px"
-                    setMobileListMenu={setMobileListMenu}
-                />
-            }
-
-
             <section
                 className="flex"
             >
                 {/* left || last-left button menu  */}
                 <LeftMenuDextop />
-
                 <section
                     className="grow bg-whitesmoke-300 min-h-[calc(100vh-60px)] rounded-l-xl lg:flex gap-4 p-3.5 pb-[3.75rem] overflow-x-scroll"
                 >
-
                     {/* main home page image text  */}
                     <EmptyPage />
-
-                    {/* right side menu/setting  */}
                 </section>
 
                 {
                     settingMenu &&
-
                     <SettingComponent
                         settingComponentHeight="262px"
                         settingComponentWidth="300px"
