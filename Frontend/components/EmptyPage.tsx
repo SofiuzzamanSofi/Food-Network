13cdf750984de4b2082f595f84ea2e9be46a5587
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-11-10 11:29:36 +0600

    EmptyPage Emai not found solved

--- Frontend/components/EmptyPage.tsx
+++ Frontend/components/EmptyPage.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
-
+import Image from "next/image";
+import emptyPage from "../assets/image-1@2x.png"
 interface EmptyPageProps {
 
 };
@@ -10,10 +11,10 @@ const EmptyPage: FC<EmptyPageProps> = ({ }) => {
             className="flex flex-col items-center justify-center text-left text-lg text-white-mode-main-text-color font-inter rounded-lg min-w-[330px] p-3 xl:p-11 bg-white z-0 w-full"
         >
             <div className="rounded-3xs bg-white flex flex-col items-center justify-center p-11 box-border gap-[42px]">
-                <img
+                <Image
                     className="relative w-[526.6px] h-[298.3px] object-cover"
                     alt=""
-                    src="/image-1@2x.png"
+                    src={emptyPage}
                 />
                 <b className="relative leading-[130%] capitalize">
                     This page is empty
